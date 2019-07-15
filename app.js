b166393b29a48cbc27ec18026540d0bde6e254dc
Nepcam <cameron.nepe@gmail.com>
2019-07-15 14:18:54 +1200

    using fetch to bring in a text.txt file and output onto html

---
+++ app.js
@@ -0,0 +1,16 @@
+document.getElementById('button1').addEventListener('click', getText);
+
+function getText() {
+  fetch('test.txt')
+  .then(function(res){
+    // console.log(res.text());
+    return res.text();
+  })
+  .then(function(data) {
+    console.log(data);
+    document.getElementById('output').innerHTML = data
+  })
+  .catch(function(err){
+    console.log(err)
+  });
+}
